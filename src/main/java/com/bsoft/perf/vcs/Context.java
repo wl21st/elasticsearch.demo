4104bd00a0293715c09c9344a482e341876527bf
Michael Luo <michael.luo.sh@gmail.com>
2015-10-12 12:34:43 -0700

    Divide the code into methods.

--- src/main/java/com/bsoft/perf/vcs/Context.java
+++ src/main/java/com/bsoft/perf/vcs/Context.java
@@ -17,6 +17,32 @@ public class Context {
 	 */
 	public static void main(String[] args) {
 
+		systemContext();
+
+		getPhysicalMemorySize();
+
+		getCPUCores();
+
+	}
+
+	private static void getCPUCores() {
+		System.out.println("CPU Cores " + Runtime.getRuntime().availableProcessors());
+	}
+
+	private static void getPhysicalMemorySize() {
+		com.sun.management.OperatingSystemMXBean os = (com.sun.management.OperatingSystemMXBean) java.lang.management.ManagementFactory
+				.getOperatingSystemMXBean();
+		long physicalMemorySize = os.getTotalPhysicalMemorySize();
+
+		System.out.println("Physical memory=" + physicalMemorySize);
+	}
+
+	/**
+	 * Interested context include the os/jvm vendor/country/hostname/user
+	 * name/timezone.
+	 */
+	private static void systemContext() {
+
 		Properties props = System.getProperties();
 
 		Map<String, Object> sortedEntries = new TreeMap<String, Object>();
@@ -28,15 +54,6 @@ public class Context {
 		for (Map.Entry<String, Object> entry : sortedEntries.entrySet()) {
 			System.out.println(entry.getKey() + "=" + entry.getValue());
 		}
-
-		com.sun.management.OperatingSystemMXBean os = (com.sun.management.OperatingSystemMXBean) java.lang.management.ManagementFactory
-				.getOperatingSystemMXBean();
-		long physicalMemorySize = os.getTotalPhysicalMemorySize();
-
-		System.out.println("Physical memory=" + physicalMemorySize);
-
-		System.out.println("CPU Cores " + Runtime.getRuntime().availableProcessors());
-
 	}
 
 }
